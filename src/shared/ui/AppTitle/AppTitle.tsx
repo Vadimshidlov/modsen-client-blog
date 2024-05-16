20d6ecb9700ed7a9385241a5648eb7ef8995e31b
Vadimshidlov <vadim.shidlov@gmail.com>
2024-05-16 17:44:28 +0300

    feat(CU-#86bymnahr): add link for app logo

--- src/shared/ui/AppTitle/AppTitle.tsx
+++ src/shared/ui/AppTitle/AppTitle.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import styles from '@/shared/ui/AppTitle/AppTitle.module.scss';
 import { useTranslations } from 'next-intl';
+import Link from 'next/link';
 
 export function AppTitle() {
   const t = useTranslations('AppTitle');
 
-  return <h1 className={styles.app__title}>{t('appTitle')}</h1>;
+  return (
+    <h1 className={styles.app__title}>
+      <Link href="/">{t('appTitle')}</Link>
+    </h1>
+  );
 }
